04c6202f84796f12a4f306473654b97857962fe6
Evan MacDonald <113189803+Evan-Prime@users.noreply.github.com>
2024-01-12 19:25:56 -0400

    Finished

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Trigger : MonoBehaviour
+{
+
+    public PlayableDirector timeline;
+
+    public GameObject player;
+
+    public bool OneTimeTrigger = false; // true = this trigger will self destruct after one use.
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+
+            // only use if you want to have an event that only triggers once
+            if (OneTimeTrigger == true)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+}
